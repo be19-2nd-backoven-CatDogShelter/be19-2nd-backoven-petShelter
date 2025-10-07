99a27aae8ad3529d9b8ef4a70110133bf2d559b3
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-10-07 14:49:26 +0900

    응답 포멧 만들기

--- catdogshelter-post-service/src/main/java/com/backoven/catdogshelter/common/response/ApiResponse.java
+++ catdogshelter-post-service/src/main/java/com/backoven/catdogshelter/common/response/ApiResponse.java
@@ -2,5 +2,15 @@ package com.backoven.catdogshelter.common.response;
 
 /* 설명. 응답 포멧(상태 코드, 메시지, 데이터 ...) */
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class ApiResponse<T> {
+    private int statusCode;
+    private String message;
+    private T data;
 }
