7a0b701a346f29512e657c31c518c5e9c98ad766
haenin <gpdnjsqhdld@naver.com>
2025-10-13 16:40:48 +0900

    user feign config
    # Conflicts:
    #	catdogshelter-user-service/build.gradle

---
+++ catdogshelter-user-service/src/main/java/com/backoven/catdogshelter/domain/user/infrastructure/PostServiceClient.java
@@ -0,0 +1,24 @@
+package com.backoven.catdogshelter.domain.user.infrastructure;
+
+import com.backoven.catdogshelter.domain.user.dto.ResponsePostDTO;
+import com.haenin.userservice.dto.ResponseOrderDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+/* 설명.
+ *  1. FeignClient관련 라이브러리 추가
+ *  2. main 가서 @EnableFeignClients 어노테이션 추가
+ *  3. FeignClientConfig 클래스 설정으로 토큰 서빙 설정할 것
+ */
+
+/* 설명. Order 도메인과의 통신을 위한 interface */
+@FeignClient(name = "CATDOGSHELTER-POST-SERVICE"
+            ,url = "localhost:8000"
+            , configuration = FeignClientConfig.class)
+public interface PostServiceClient {
+    @GetMapping("/order-service/users/{userId}/orders")
+    List<ResponsePostDTO> getUserPosts(@PathVariable String userId);
+}
