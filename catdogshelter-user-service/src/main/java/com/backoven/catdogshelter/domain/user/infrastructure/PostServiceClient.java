c2c58f297f6ef644e9c6342f140b52700a2fa5ea
haenin <gpdnjsqhdld@naver.com>
2025-10-14 15:58:05 +0900

    user-feign 개발중

--- catdogshelter-user-service/src/main/java/com/backoven/catdogshelter/domain/user/infrastructure/PostServiceClient.java
+++ catdogshelter-user-service/src/main/java/com/backoven/catdogshelter/domain/user/infrastructure/PostServiceClient.java
@@ -1,11 +1,14 @@
 package com.backoven.catdogshelter.domain.user.infrastructure;
 
+import com.backoven.catdogshelter.domain.user.query.dto.ResponsePostDTO;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+/* 설명. Post 도메인과의 통신을 위한 interface */
 @FeignClient(name = "CATDOGSHELTER-POST-SERVICE"
             ,url = "localhost:8000"
             , configuration = FeignClientConfig.class)
@@ -13,3 +16,4 @@ public interface PostServiceClient {
     @GetMapping("/post-service/mypage/{userId}/posts")
     List<ResponsePostDTO> getUserPosts(@PathVariable String userId);
 }
+
