37763db6679350fe87555f9cd0db3de78f63f523
Byun-Mincheol <150311355+bynmch@users.noreply.github.com>
2025-10-24 11:59:24 +0900

    gateway cors용 코드 추가 (#198)
    * 봉사게시판 및 공지 게시판 신고 카테고리 import 수정
    
    * ddl 수정본 추가
    
    - 봉사모임 파일 테이블
    - 공지 unique 제약 조건 수정
    - syntax 에러 수정
    - 기본 더미 데이터 수정
    
    * 봉사 및 공지 더미 데이터 수정
    
    * cors 해결위한 코드 및 dto
    
    - userdtotodtomapper.java 없던 파일 추가
    - cors 해결 위한 설정 bean 추가
    - gateway에 cors 해결위한 yml 코드 추가

--- catdogshelter-user-service/src/main/java/com/backoven/catdogshelter/domain/shelterhead/security/ShelterheadWebSecurity.java
+++ catdogshelter-user-service/src/main/java/com/backoven/catdogshelter/domain/shelterhead/security/ShelterheadWebSecurity.java
@@ -19,7 +19,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -86,5 +90,20 @@ public class ShelterheadWebSecurity {
      */
     private Filter getAuthenticationFilter(AuthenticationManager authenticationManager) {
         return new ShelterheadAuthenticationFilter(authenticationManager, env);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedOriginPattern("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
