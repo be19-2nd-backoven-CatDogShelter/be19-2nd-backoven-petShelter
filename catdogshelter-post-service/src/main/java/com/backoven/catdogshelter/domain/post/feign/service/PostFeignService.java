3fc6bcd08ad363c1a3c1d4406e78afd09508dad1
haenin <gpdnjsqhdld@naver.com>
2025-10-27 09:21:40 +0900

    adoption 수정 중

--- catdogshelter-post-service/src/main/java/com/backoven/catdogshelter/domain/post/feign/service/PostFeignService.java
+++
@@ -1,9 +0,0 @@
-package com.backoven.catdogshelter.domain.post.feign.service;
-
-import com.backoven.catdogshelter.domain.post.query.dto.PostInventoryDTO;
-
-import java.util.List;
-
-public interface PostFeignService {
-    List<PostInventoryDTO> getPostByUserId(String userId);
-}
