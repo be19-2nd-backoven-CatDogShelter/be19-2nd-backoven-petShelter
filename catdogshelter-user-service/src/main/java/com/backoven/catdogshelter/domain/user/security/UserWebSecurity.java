f8af17771b326f451009fe49d2fddd7e5af74022
LeeSeonggeon <tmdrjs0040@gmail.com>
2025-10-28 12:17:54 +0900

    1. mapper 수정 2. gateway yml 수정 3. 관리자 페이지 권한 전체 허용(나중에 수정 필요)

--- catdogshelter-user-service/src/main/java/com/backoven/catdogshelter/domain/user/security/UserWebSecurity.java
+++ catdogshelter-user-service/src/main/java/com/backoven/catdogshelter/domain/user/security/UserWebSecurity.java
@@ -80,10 +80,11 @@ public class UserWebSecurity {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/user/regist").permitAll() // 회원가입 허용
-                        .requestMatchers(HttpMethod.PATCH,"/user/admi/ratingId").hasAuthority("ROLE_MASTER_ADMIN")
-                        .requestMatchers(HttpMethod.PUT,"/user/admin/**").hasAuthority("ROLE_ADMIN")
-                        .requestMatchers(HttpMethod.GET,"/user/admin/admin").hasAuthority("ROLE_MASTER_ADMIN")
-                        .requestMatchers(HttpMethod.GET,"/user/admin/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/user/admin/**").permitAll()
+//                        .requestMatchers(HttpMethod.PATCH,"/user/admi/ratingId").hasAuthority("ROLE_MASTER_ADMIN")
+//                        .requestMatchers(HttpMethod.PUT,"/user/admin/**").hasAuthority("ROLE_ADMIN")
+//                        .requestMatchers(HttpMethod.GET,"/user/admin/admin").hasAuthority("ROLE_MASTER_ADMIN")
+//                        .requestMatchers(HttpMethod.GET,"/user/admin/**").hasAuthority("ROLE_ADMIN")
                         .requestMatchers(HttpMethod.POST, "/user/login").permitAll() // 로그인 허용
                         .requestMatchers(HttpMethod.POST, "/user/password/**").permitAll() // 비밀번호 찾기
                         .requestMatchers(HttpMethod.POST, "/user/find-id/**").permitAll() // 아이디 찾기
